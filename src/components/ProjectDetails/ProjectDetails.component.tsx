673a99f916d52ffdd61f8280d1c592d02ec2aced
BrianBTI <bertili.brian@gmail.com>
2022-01-21 11:54:45 +0100

    💄  & improve functionality

--- src/components/ProjectDetails/ProjectDetails.component.tsx
+++ src/components/ProjectDetails/ProjectDetails.component.tsx
@@ -3,8 +3,9 @@ import { useParams } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ProjectDetailsProps } from './ProjectDetails.props';
 import { UPDATE_PROJECT } from 'services/projects.service';
-import './ProjectDetails.style.scss';
 import { Card } from 'components';
+import { BsFillCheckCircleFill } from 'react-icons/bs';
+import './ProjectDetails.style.scss';
 
 export const ProjectCard = ({ project }: ProjectDetailsProps) => {
     const createdAt = 'test';
@@ -17,9 +18,11 @@ export const ProjectCard = ({ project }: ProjectDetailsProps) => {
         'Jenny Doe',
         'Juan Dos Tres'
     ];
-    const [isEditMode, setIsEditMode] = useState(false);
-    const toggleEditMode = () => { setIsEditMode(!isEditMode); };
-    const [newProjectInfos, setNewProjectInfos] = useState({ name: project.name.toUpperCase(), description: project.description });
+    const [isEditModeName, setIsEditModeName] = useState(false);
+    const [isEditModeDescription, setIsEditModeDescription] = useState(false);
+    const toggleEditModeName = () => { setIsEditModeName(!isEditModeName); };
+    const toggleEditModeDescription = () => { setIsEditModeDescription(!isEditModeDescription); };
+    const [newProjectInfos, setNewProjectInfos] = useState({ name: project.name, description: project.description });
     const { id: projectId } = useParams < { id: string }>();
     const [updateProject] = useMutation(UPDATE_PROJECT);
 
@@ -34,17 +37,18 @@ export const ProjectCard = ({ project }: ProjectDetailsProps) => {
                 }
             }
         });
-        setIsEditMode(!isEditMode);
+        setIsEditModeName(false);
+        setIsEditModeDescription(false);
     };
 
     return (
         <div className="project-details">
             <div className="project-details__header">
-                {!isEditMode
+                {!isEditModeName
                     ? (
                         <div
-                            className="project-details__header__name"
-                            onClick={toggleEditMode}
+                            className="project-details__header__name hoverEdit"
+                            onClick={toggleEditModeName}
                         >
                             {project.name.toUpperCase()}
                         </div>
@@ -62,15 +66,16 @@ export const ProjectCard = ({ project }: ProjectDetailsProps) => {
                                         setNewProjectInfos({ name: e.target.value, description: newProjectInfos.description });
                                     }}
                                 />
-                                <button
-                                    type="submit"
-                                    style={{
-                                        marginLeft: '10px',
-                                        paddingInline: '10px'
-                                    }}
-                                >
-                                Confirmer
-                                </button>
+                                { isEditModeName &&
+                                     (
+                                         <button
+                                             type="submit"
+                                             className="name__icon"
+                                         >
+                                             <BsFillCheckCircleFill size={20} color='green' />
+                                         </button>)
+
+                                }
                             </form>
                         </div>
                     )}
@@ -99,21 +104,35 @@ export const ProjectCard = ({ project }: ProjectDetailsProps) => {
                         <div className="project-details__body__description__title">
                             Description
                         </div>
-                        {!isEditMode
+                        {!isEditModeDescription
                             ? (
-                                <div className="project-details__body__description__value">
+                                <div className="project-details__body__description__value hoverEdit" onClick={toggleEditModeDescription}>
                                     {project.description}
                                 </div>
                             )
                             : (
-                                <input
-                                    type="text"
-                                    style={{ fontSize: 'x-large' }}
-                                    defaultValue={project.description}
-                                    onChange={(e) => {
-                                        setNewProjectInfos({ name: newProjectInfos.name, description: e.target.value });
-                                    }}
-                                />
+                                <form
+                                    onSubmit={submitUpdatedProjectInfos}
+                                >
+                                    <input
+                                        type="text"
+                                        style={{ fontSize: 'x-large' }}
+                                        defaultValue={project.description}
+                                        onChange={(e) => {
+                                            setNewProjectInfos({ name: newProjectInfos.name, description: e.target.value });
+                                        }}
+                                    />
+                                    { isEditModeDescription &&
+                                     (
+                                         <button
+                                             type="submit"
+                                             className="description__icon"
+                                         >
+                                             <BsFillCheckCircleFill size={20} color='green' />
+                                         </button>)
+
+                                    }
+                                </form>
                             )}
                     </div>
                 </div>
