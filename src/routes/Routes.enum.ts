e17046d5efb98f181a7c1824172c79fb748da0b1
Amine Abdelli <amine.abdelli@outlook.fr>
2022-04-28 13:23:19 +0200

    feat(reset-password): Create ResetPassword component + Query/Mutation needed

--- src/routes/Routes.enum.ts
+++ src/routes/Routes.enum.ts
@@ -3,8 +3,9 @@ export enum Routes {
     HOME = '/',
     LOGIN = '/login',
     SIGNUP = '/signup',
-    FORGOT_PASSWORD = '/forgotPassword',
+    FORGOT_PASSWORD = '/forgot_password',
     PROJECTS = '/projects',
     PROJECT = '/project/:id',
     CONFIRM_ACCOUNT = '/confirm_account&token=:token',
+    RESET_PASSWORD = '/reset_password&token=:token',
 }
